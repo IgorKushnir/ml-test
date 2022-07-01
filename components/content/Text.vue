e7bf00ad2e69df80a604840defdee3797bab8fc4
rodney <rodney@sarakuz.com>
2022-07-01 11:52:26 +0300

    Markdown styles 2

--- components/content/Text.vue
+++ components/content/Text.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-html="data"/>
+  <div class="text-content" v-html="data"/>
 </template>
 
 <script setup>
@@ -18,7 +18,7 @@ const props = defineProps({
 </script>
 
 <style lang="scss" scoped>
->>> .history {
+.text-content >>> .history {
   column-count: 2;
   color: $gray;
 
@@ -37,7 +37,7 @@ const props = defineProps({
     color: $dark-blue;
   }
 }
->>> .key {
+.text-content >>>  .key {
   display: grid;
   grid-gap: 40px;
   grid-template-columns: repeat(3, 1fr);
@@ -56,7 +56,7 @@ const props = defineProps({
   }
 }
 @include md {
-  >>> .history {
+  .text-content >>> .history {
     column-count: 1;
     &  > div {
       padding-top: 16px;
@@ -68,7 +68,7 @@ const props = defineProps({
     }
   }
 
-  >>> .key {
+  .text-content >>>  .key {
     grid-gap: 20px;
     p {
       line-height: 1.2em;
