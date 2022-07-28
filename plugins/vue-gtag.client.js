68cd901172e76b1d79c63ec8871937d12aceaeb9
rodney <rodney@sarakuz.com>
2022-07-28 18:43:17 +0300

    Added Google analitics

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,27 +1,27 @@
 import VueGtag from 'vue-gtag-next'
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//     console.log('Google analytics: G-XJ36V6PWXJ');
-//     nuxtApp.vueApp.use(VueGtag, {
-//         property: {
-//             id: 'G-XJ36V6PWXJ'
-//         }
-//     })
-//
-//
-// })
 export default defineNuxtPlugin((nuxtApp) => {
-        console.log('Google analytics: G-XJ36V6PWXJ - v2');
-
-    const getGDPR = localStorage.getItem('GDPR:accepted');
-    console.log({getGDPR});
+    console.log('Google analytics: G-XJ36V6PWXJ');
     nuxtApp.vueApp.use(VueGtag, {
         property: {
             id: 'G-XJ36V6PWXJ'
-        },
-        appName: 'Milla local',
-        // enabled: getGDPR === 'true',
-        enabled: true,
-        pageTrackerScreenviewEnabled: true
-    }, nuxtApp.vueApp.router);
-});
+        }
+    })
+
+
+})
+// export default defineNuxtPlugin((nuxtApp) => {
+//         console.log('Google analytics: G-XJ36V6PWXJ - v2');
+//
+//     const getGDPR = localStorage.getItem('GDPR:accepted');
+//     console.log({getGDPR});
+//     nuxtApp.vueApp.use(VueGtag, {
+//         property: {
+//             id: 'G-XJ36V6PWXJ'
+//         },
+//         appName: 'Milla local',
+//         // enabled: getGDPR === 'true',
+//         enabled: true,
+//         pageTrackerScreenviewEnabled: true
+//     }, nuxtApp.vueApp.router);
+// });
