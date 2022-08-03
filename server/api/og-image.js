7dbc32e4c0d9913856a1bec33c3b928db2a8a1ef
rodney <rodney@sarakuz.com>
2022-08-03 19:48:52 +0300

    og fix

--- server/api/og-image.js
+++ server/api/og-image.js
@@ -1,6 +1,6 @@
 import renderSocialImage from "puppeteer-social-image";
 import puppeteer from "puppeteer";
-
+import chromium  from 'chrome-aws-lambda';
 
 export default  defineEventHandler(async (event) => {
 
@@ -9,6 +9,8 @@ export default  defineEventHandler(async (event) => {
 
     const title = urlParams.get('title');
     const image = urlParams.get('image');
+
+    console.log( puppeteer.executablePath(), '--- Browser ---');
 
     const img = await renderSocialImage.default({
         // template: "basic",
@@ -39,11 +41,24 @@ export default  defineEventHandler(async (event) => {
         output: "image.png",
         size: "facebook",
         browser: await puppeteer.launch({
+            executablePath: puppeteer.executablePath(),
+            headless: true
             // executablePath: '/Users/rodney/Documents/Dev/projects/millanova-frontend/node_modules/puppeteer/.local-chromium/mac-1022525/chrome-mac/Chromium.app/Contents/MacOS/Chromium',
             // executablePath: __dirname + '/../../puppeteer/.local-chromium/mac-1022525/chrome-mac/Chromium.app/Contents/MacOS/Chromium',
             // executablePath: __dirname + '/../../puppeteer/.local-chromium/linux-1022525/chrome-linux/chrome',
-            executablePath: '/workspace/node_modules/puppeteer/.local-chromium/linux-1022525/chrome-linux/chrome',
+            // executablePath: '/workspace/node_modules/puppeteer/.local-chromium/linux-1022525/chrome-linux/chrome',
         })
+        // browser: await chromium.puppeteer.launch({
+        //     args: chromium.args,
+        //     defaultViewport: chromium.defaultViewport,
+        //     executablePath: await chromium.executablePath,
+        //     headless: chromium.headless,
+        //     ignoreHTTPSErrors: true,
+        //     // executablePath: '/Users/rodney/Documents/Dev/projects/millanova-frontend/node_modules/puppeteer/.local-chromium/mac-1022525/chrome-mac/Chromium.app/Contents/MacOS/Chromium',
+        //     // executablePath: __dirname + '/../../puppeteer/.local-chromium/mac-1022525/chrome-mac/Chromium.app/Contents/MacOS/Chromium',
+        //     // executablePath: __dirname + '/../../puppeteer/.local-chromium/linux-1022525/chrome-linux/chrome',
+        //     // executablePath: '/workspace/node_modules/puppeteer/.local-chromium/linux-1022525/chrome-linux/chrome',
+        // })
         // preview: true
     })
     return img
