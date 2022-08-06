93c3b2c4de85d03ee9000243fe02e41dd39ae88c
rodney <rodney@sarakuz.com>
2022-08-06 23:17:13 +0300

    og-image

--- components/Seo.vue
+++ components/Seo.vue
@@ -12,22 +12,51 @@ const props = defineProps({
   title: {
     type: String,
     required: false
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  image: {
+    type: String,
+    required: false
   }
 })
 const defaultTitle = ref('Milla Nova')
 
 const translations = useTranslationsData();
 
+const fullPath = useRoute().fullPath;
+const url = "https://millanova.com" + fullPath
+
+
+const title = props.data?.seo?.metaTitle ?? props.data?.title ?? props.title ?? defaultTitle.value;
+const description = props.description ?? "millanova.com";
+
+const ogImageQuery = new URLSearchParams({
+  description: description,
+  title: title,
+  image: props.image,
+});
+
 function setSeo() {
   useHead({
-    title: props.data?.seo?.metaTitle ?? props.data?.title ?? props.title ?? defaultTitle.value,
+    title: title,
     titleTemplate: (title) => `${title} - ${translations.value.site_title}`,
+    link: [
+      { rel: 'canonical', href: url}
+    ],
     meta: [
-      false ? {
-        hid: 'description',
-        name: 'description',
-        content: 'dfdf'
-      } : {}
+      { property: 'og:locale', content: 'en_US' },
+      { property: 'og:title', content: title },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:image', content: 'en_US' },
+      { property: 'og:description', content: description },
+      { property: 'og:url', content: "http://159.223.121.185/?" + ogImageQuery  },
+      { property: 'twitter:card', content: 'summary_large_image' },
+
+      { hid: 'description', name: 'description', content: description }
+
     ],
   })
 }
