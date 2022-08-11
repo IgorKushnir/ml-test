4300fd6ad8a740effd6609afbe71fe01d13c4456
rodney <rodney@sarakuz.com>
2022-08-11 16:54:27 +0300

    Seo

--- components/Seo.vue
+++ components/Seo.vue
@@ -22,7 +22,6 @@ const props = defineProps({
     required: false
   }
 })
-const defaultTitle = ref('Milla Nova')
 
 const translations = useTranslationsData();
 
@@ -30,20 +29,25 @@ const fullPath = useRoute().fullPath;
 const siteUrl = "https://millanova.com" + fullPath
 
 
-const title = props.data?.seo?.metaTitle ?? props.data?.title ?? props.title ?? defaultTitle.value;
-const description = props.description ?? "millanova.com";
+const title =
+    props.data?.seo?.metaTitle ??
+    props.data?.title ??
+    props.title ??
+    translations.value.site_title;
+const description =
+    props.data?.seo?.metaDescription ??
+    props.data?.text ??
+    props.data?.description ??
+    translations.value.site_description;
 
-// const ogImageQuery = new URLSearchParams({
-//   description: description,
-//   title: title,
-//   image: props.image,
-// });
-// const ogImageUrl = "http://159.223.121.185/?" +ogImageQuery
+const ogImageUrl = 'https://millanova.com/img/og-image.jpg'
+
+
 
     function setSeo() {
   useHead({
     title: title,
-    titleTemplate: (title) => `${title} - ${translations.value.site_title}`,
+    titleTemplate: (title) => `${ title } - ${ description }`,
     link: [
       { rel: 'canonical', href: siteUrl}
     ],
@@ -52,16 +56,19 @@ const description = props.description ?? "millanova.com";
       { property: 'og:site_name', content: 'Milla Nova' },
       { property: 'og:title', content: title },
       { property: 'og:type', content: 'website' },
-      // { property: 'og:image', content: ogImageUrl },
       { property: 'og:description', content: description },
       { property: 'og:url', content:  siteUrl },
       { property: 'twitter:card', content: 'summary_large_image' },
-      // { property: 'twitter:image', content: ogImageUrl },
+      { property: 'og:image', content: ogImageUrl },
+      { property: 'twitter:image', content: ogImageUrl },
 
 
       { hid: 'description', name: 'description', content: description }
 
     ],
+    htmlAttrs: [
+      {prefix: "og: http://ogp.me/ns#"}
+    ]
   })
 }
 watch(() => props.data, () => {
