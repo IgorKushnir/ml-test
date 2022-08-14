2f68864a6bb8a870dedbdece44448daf25b101b1
rodney <rodney@sarakuz.com>
2022-08-14 16:14:49 +0300

    Preview modo 2

--- server/api/preview.js
+++ server/api/preview.js
@@ -13,32 +13,32 @@ export default async  (req, res, next) => {
 
     if (q.get('type') === 'product') {
         const slug = q.get('slug')
-        const _url = host + '/product/' + slug + '?draft=true';
+        const _url = '/product/' + slug + '?draft=true';
         res.end(iFrame(_url))
 
     }
 
     if (q.get('type') === 'page') {
         const slug = q.get('slug')
-        const _url = host + '/' + slug + '?draft=true';
+        const _url = '/' + slug + '?draft=true';
         res.end(iFrame(_url))
     }
 
     if (q.get('type') === 'trunk-shows') {
         const slug = q.get('slug')
-        const _url = host + '/trunk-shows/' + slug + '?draft=true';
+        const _url = '/trunk-shows/' + slug + '?draft=true';
         res.end(iFrame(_url))
     }
 
     if (q.get('type') === 'inspiration') {
         const slug = q.get('slug')
-        const _url = host + '/inspiration/' + slug + '?draft=true';
+        const _url = '/inspiration/' + slug + '?draft=true';
         res.end(iFrame(_url))
     }
 
     if (q.get('type') === 'news') {
         const slug = q.get('slug')
-        const _url = host + '/news/' + slug + '?draft=true';
+        const _url = '/news/' + slug + '?draft=true';
         res.end(iFrame(_url))
     }
 
@@ -57,6 +57,7 @@ function iFrame(url) {
 <body style="margin: 0"><iframe 
     id="inlineFrameExample"
     style="border: none"
+    sandbox="allow-scripts allow-same-origin allow-scripts allow-popups allow-forms allow-pointer-lock"
     title="Inline Frame Example"
     width="100%"
     height="100%"
