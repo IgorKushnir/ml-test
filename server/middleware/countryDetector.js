0770fd46fe8dff1fe09ef943663e67816729b131
rodney <rodney@sarakuz.com>
2023-08-29 22:21:49 +0300

    Store finder get gcountry by IP

--- server/middleware/countryDetector.js
+++ server/middleware/countryDetector.js
@@ -2,24 +2,32 @@ import axios from 'axios';
 
 export default defineEventHandler((event) => {
     const headers = getRequestHeaders(event)
-    const ip = headers['x-forwarded-for']
+    let ip = headers['x-forwarded-for']
     const config = useRuntimeConfig();
     const apiKey = config.ABSTRACT_API
 
+    ip = '2001:871:64:5fa:49fc:56c3:8dd7:fe7c,172.68.50.87'.split(',')
+    ip = ip[ip.length-1] ?? null
     const url = "https://ipgeolocation.abstractapi.com/v1/?api_key="+apiKey+"&ip_address="+ip
     // const url = "https://ipgeolocation.abstractapi.com/v1/?api_key=3cfbdffdc28f4360988300e9663c1f00&ip_address=92.184.105.98"
 
-    axios.get(url)
-        .then(response => {
-            const geo = response.data;
-            event.context.country = geo.country_code
-            setCookie(event, 'country', geo.country_code)
-        })
-        .catch(error => {
-            event.context.country = null
-            setCookie(event, 'country', null)
-            console.log(error);
-        });
+    if (ip) {
+        axios.get(url)
+            .then(response => {
+                const geo = response.data;
+                event.context.country = geo.country_code
+                setCookie(event, 'country', geo.country_code)
+            })
+            .catch(error => {
+                event.context.country = null
+                setCookie(event, 'country', null)
+                console.log(error);
+            });
+    } else {
+        event.context.country = null
+        setCookie(event, 'country', null)
+    }
+
 
 
 })
