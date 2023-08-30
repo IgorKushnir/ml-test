5456fd3b5da3bc207af771222572f1e4b1b003b0
rodney <rodney@sarakuz.com>
2023-08-30 22:31:31 +0300

    Store finder get gcountry by IP

--- server/api/countryDetectorTest.js
+++ server/api/countryDetectorTest.js
@@ -1,19 +1,21 @@
 import axios from 'axios';
 
 export default defineEventHandler(async (event) => {
+    // return getRequestIP(event, { xForwardedFor: true });
     const headers = getRequestHeaders(event)
     let ip = headers['x-forwarded-for']
     const config = useRuntimeConfig();
     const apiKey = config.ABSTRACT_API
 
     ip = ip.split(',')
-    ip = ip[ip.length-1] ?? null
+    ip = ip[0] ?? null
     const url = "https://ipgeolocation.abstractapi.com/v1/?api_key="+apiKey+"&ip_address="+ip
 
 
     const response = await axios.get(url)
     return {
         ip: ip,
+        ips: headers['x-forwarded-for'],
         abstract: response.data
     }
 
