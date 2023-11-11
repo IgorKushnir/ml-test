01f0737a47d354c2b0852a320f60aa8c01ff311b
rodney <rodney@sarakuz.com>
2023-11-11 22:12:50 +0200

    Fixed country name sent to speadsheet

--- components/store/StoreItem.vue
+++ components/store/StoreItem.vue
@@ -39,8 +39,7 @@
           <div class="subheader small m-t-32">Lines</div>
           <p  class="p-small" >{{store.lines.data.map(line => line.attributes.title).join(', ')   }}</p>
         </template>
-
-        <Button v-if="store.email" :path="'/book-an-appointment?q=' + encodeToBase64(JSON.stringify([route.query.country ?? store.country_code, store.city.toLowerCase(), store.title + ' (' +store.address + ')', store.email]))" class="m-t-32">Book an appointment</Button>
+        <Button v-if="store.email" :path="'/book-an-appointment?q=' + encodeToBase64(JSON.stringify([route.params.country ?? store.country_code, store.city.toLowerCase(), store.title + ' (' +store.address + ')', store.email]))" class="m-t-32">Book an appointment</Button>
 
       </div>
 
@@ -59,6 +58,9 @@ defineProps({
   store: {
     type: Object,
     required: true
+  },
+  countrySlug: {
+    type: String,
   },
   country: {
     type: Boolean,
