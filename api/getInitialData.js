181d876a31296bf5660da6400a931b00f0734f06
rodney <rodney@sarakuz.com>
2024-02-27 00:26:00 +0200

    pl

--- api/getInitialData.js
+++ api/getInitialData.js
@@ -57,7 +57,7 @@ export default async function (lang) {
                 }
             }
         }
-        lines (sort: "order") {
+        lines (sort: "order" locale:"${lang}") {
             data {
                 attributes {
                     title
