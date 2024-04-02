89b75f7847af150957ed358966084b91079d2b61
rodney <rodney@sarakuz.com>
2024-04-02 14:54:02 +0300

    Page not found status 404

--- components/PageNotFound.vue
+++ components/PageNotFound.vue
@@ -17,21 +17,32 @@ const props = defineProps({
     required: true
   }
 })
-
+const event = useRequestEvent()
 if (process.server && props.show) {
   const nuxtApp = useNuxtApp()
   const error = new Error()
   error.statusCode = 404
   nuxtApp.ssrContext.error = error
 
+  // console.log('------', event);
+  if (event) {
+    setResponseStatus(event, 404, 'Page Not Found')
+  }
   // throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
 
 
-  if (nuxtApp.ssrContext) {
-    nuxtApp.ssrContext.nuxt.error = createError({ statusCode: 404 });
-  }
+  // if (nuxtApp.ssrContext) {
+  //   nuxtApp.ssrContext.nuxt.error = createError({ statusCode: 404 });
+  // }
 
 }
+// if (props.show) {
+//   // if (nuxtApp.ssrContext) {
+//   //   console.log(nuxtApp.ssrContext.nuxt);
+//   //   // nuxtApp.ssrContext.nuxt.error = createError({ statusCode: 404 });
+//   // }
+// }
+
 </script>
 
 <style scoped>
