101b02bc06f454f5151c20a984c100608fe7951f
Igor Kushnir <51193240+IgorKushnir@users.noreply.github.com>
2024-10-26 15:22:49 +0300

    Remove Pinterest code

--- app.vue
+++ app.vue
@@ -81,7 +81,7 @@ useHead({
 
   script: [
     {
-      innerHTML: `!function(f,b,e,v,n,t,s)
+      innerHTML: process.env.FACEBOOK_INIT ? `!function(f,b,e,v,n,t,s)
                   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
                   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
                   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -89,30 +89,30 @@ useHead({
                   t.src=v;s=b.getElementsByTagName(e)[0];
                   s.parentNode.insertBefore(t,s)}(window, document,'script',
                   'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '406315495262476');
+                  fbq('init', ${process.env.FACEBOOK_INIT});
                   fbq('track', 'PageView');
-                  `
+                  ` : ''
     },
     { // Pinterest Tag // posthog.capture('my event', { property: 'value' })
-      innerHTML: `!function(e){if(!window.pintrk){window.pintrk = function () {
+      innerHTML: process.env.PINTEREST_ID ? `!function(e){if(!window.pintrk){window.pintrk = function () {
                   window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var
                     n=window.pintrk;n.queue=[],n.version="3.0";var
                     t=document.createElement("script");t.async=!0,t.src=e;var
                     r=document.getElementsByTagName("script")[0];
                     r.parentNode.insertBefore(t,r)}}("https://s.pinimg.com/ct/core.js");
-                  pintrk('load', '2613056388970', {em: '<user_email_address>'});
+                  pintrk('load', ${process.env.PINTEREST_ID}, {em: '<user_email_address>'});
                   pintrk('page');
-                  `
+                  ` : ''
     },
-    {innerHTML: `(function(h,o,t,j,a,r){
+    {innerHTML: process.env.HOTJAR_ID ? `(function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:5179312,hjsv:6};
+                h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`},
-    {async: true, src:`//cdn.cookie-script.com/s/${process.env.COOKIEYES_CODE}.js`}
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');` : ''},
+    {async: true, src:`//cdn.cookie-script.com/s/${process.env.COOKIEYES_CODE}.js`},
     // { async: true, src: "https://www.googletagmanager.com/gtag/js?id=AW-16455473849"},
     // {
     //   innerHTML: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-16455473849');`
@@ -120,11 +120,11 @@ useHead({
   ],
   noscript: [
     {
-      innerHTML: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1040268723752792&ev=PageView&noscript=1" />`
+      innerHTML: process.env.FACEBOOK_ID ? `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=${process.env.FACEBOOK_ID}&ev=PageView&noscript=1" />` : ''
       // tagPosition: 'bodyOpen'
     },
     { // Pinterest Tag
-      innerHTML: `<img height="1" width="1" style="display:none;" alt="" src="https://ct.pinterest.com/v3/?event=init&tid=2613056388970&pd[em]=<hashed_email_address>&noscript=1" />`
+      innerHTML: process.env.PINTEREST_ID ? `<img height="1" width="1" style="display:none;" alt="" src="https://ct.pinterest.com/v3/?event=init&tid=${process.env.PINTEREST_ID}&pd[em]=<hashed_email_address>&noscript=1" />` : ''
     },
   ],
 })
