5cedc5acd3579a355459ec7fa3ccf349a4b14b25
Igor Kushnir <51193240+IgorKushnir@users.noreply.github.com>
2024-11-05 19:33:03 +0200

    Remove inspiracija

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
             '/collections', '/pl/kolekcje',
             '/store-finder', '/pl/wyszukiwarka-sklepow',
             '/company', '/pl/firma',
-            '/inspiration', '/pl/inspiracja' ],
+            '/inspiration' ],
       },
     },
   css: [
