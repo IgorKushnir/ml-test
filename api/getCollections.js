402f9a0b03702b517e87bf3a8ddaf582e15a1cbf
Igor Kushnir <51193240+IgorKushnir@users.noreply.github.com>
2024-11-06 12:18:12 +0200

    Change collections sort

--- api/getCollections.js
+++ api/getCollections.js
@@ -17,7 +17,7 @@ query Collections{
             slug
             order
             collections (
-              sort: "order"
+              sort: "createdAt"
             ) {
               data {
                 attributes {
@@ -43,7 +43,7 @@ query Collections{
 
     const { data, pending, refresh, error } = await useLazyAsyncData('l_data_'+collection, () => response, {
         transform: (d) => {
-            const data = d?.data[collection]?.data;
+            const data = d.data[collection].data;
             let lines = data.map(line => line.attributes)
 
             return lines;
